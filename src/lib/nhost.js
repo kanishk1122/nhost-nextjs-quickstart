60f71c449d7bcea976351d4a71032e2583978ce1
kanishksonifyt <kanishksoni_fyt@outlook.com>
2025-08-12 13:25:21 +0530

    fix: provide default values for NhostClient subdomain and region

--- src/lib/nhost.js
+++ src/lib/nhost.js
@@ -1,6 +1,6 @@
 import { NhostClient } from "@nhost/react";
 
 export const nhost = new NhostClient({
-  subdomain: process.env.NEXT_PUBLIC_NHOST_SUBDOMAIN,
-  region: process.env.NEXT_PUBLIC_NHOST_REGION,
+  subdomain: process.env.NEXT_PUBLIC_NHOST_SUBDOMAIN || "subdomain",
+  region: process.env.NEXT_PUBLIC_NHOST_REGION || "region",
 });
